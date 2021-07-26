f71eefa233f8d893d7fc2a8a8e3bb6ae2a80c173
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-07-26 14:55:42 +0100

    chatbot

--- include/footer.php
+++ include/footer.php
@@ -53,31 +53,38 @@
         </div>
     </div>
 </footer>
-<!-- Messenger Chat Plugin Code -->
-<div id="fb-root"></div>
-
-<!-- Your Chat Plugin code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
 <script>
-var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "105427265145667");
-chatbox.setAttribute("attribution", "biz_inbox");
-
-window.fbAsyncInit = function() {
-    FB.init({
-        xfbml: true,
-        version: 'v11.0'
-    });
+var MessageBirdChatWidgetSettings = {
+    widgetId: '5388d685-ca07-4e3f-9469-8c467871d56d',
+    initializeOnLoad: true,
 };
-
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+! function() {
+    "use strict";
+    if (Boolean(document.getElementById("live-chat-widget-script"))) console.error(
+        "MessageBirdChatWidget: Snippet loaded twice on page");
+    else {
+        var e, t;
+        window.MessageBirdChatWidget = {}, window.MessageBirdChatWidget.queue = [];
+        for (var i = ["init", "setConfig", "toggleChat", "identify", "hide", "on", "shutdown"], n = function() {
+                var e = i[d];
+                window.MessageBirdChatWidget[e] = function() {
+                    for (var t = arguments.length, i = new Array(t), n = 0; n < t; n++) i[n] = arguments[n];
+                    window.MessageBirdChatWidget.queue.push([
+                        [e, i]
+                    ])
+                }
+            }, d = 0; d < i.length; d++) n();
+        var a = (null === (e = window) || void 0 === e || null === (t = e.MessageBirdChatWidgetSettings) || void 0 ===
+                t ? void 0 : t.widgetId) || "",
+            o = function() {
+                var e, t = document.createElement("script");
+                t.type = "text/javascript", t.src = "https://livechat.messagebird.com/bootstrap.js?widgetId=".concat(a),
+                    t.async = !0, t.id = "live-chat-widget-script";
+                var i = document.getElementsByTagName("script")[0];
+                null == i || null === (e = i.parentNode) || void 0 === e || e.insertBefore(t, i)
+            };
+        "complete" === document.readyState ? o() : window.attachEvent ? window.attachEvent("onload", o) : window
+            .addEventListener("load", o, !1)
+    }
+}();
 </script>
