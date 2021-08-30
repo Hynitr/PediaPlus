06f482a9f14d0d7cba99a1ef7e45b4feb4a683ff
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-08-30 14:01:35 +0100

    google analytics

--- include/header.php
+++ include/header.php
@@ -29,6 +29,16 @@
 
    <link type="image/x-icon" rel="shortcut icon" href="images/ico.png">
 
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3KV24C6DT1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3KV24C6DT1');
+</script>
+
    </head>
 
    <body>
